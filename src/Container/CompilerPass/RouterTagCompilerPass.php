2f0b44a673ede5d8f165a2a240231d1e7ed68ca3
unwarysheep <unwarysheep@icloud.com>
2016-03-03 12:18:59 +0000

    Refactoring code
    Change Yaml config files to XML and improve interoperability by adding
    ability to tag event listeners and subscribers instead of implicitly
    setting them on the event dispatcher component

--- src/Container/CompilerPass/RouterTagCompilerPass.php
+++ src/Container/CompilerPass/RouterTagCompilerPass.php
@@ -9,6 +9,24 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class RouterTagCompilerPass implements CompilerPassInterface
 {
+    protected $service;
+    
+    protected $serviceTag;
+    
+    /**
+     * __construct function.
+     * 
+     * @access public
+     * @param string $service (default: 'route')
+     * @param string $serviceTag (default: 'route_collection')
+     * @return void
+     */
+    public function __construct(string $service = 'route', string $serviceTag = 'route_collection')
+    {
+        $this->service = $service;
+        $this->serviceTag = $serviceTag;
+    }
+    
     /**
      * You can modify the container here before it is dumped to PHP code.
      *
@@ -16,28 +34,33 @@ class RouterTagCompilerPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $routeTags = $container->findTaggedServiceIds('route');
+        $routeTags = $container->findTaggedServiceIds($this->service);
 
-        $collectionTags = $container->findTaggedServiceIds('route_collection');
+        $collectionTags = $container->findTaggedServiceIds($this->serviceTag);
 
         /** @var Definition[] $routeCollections */
         $routeCollections = array();
-        foreach ($collectionTags as $serviceName => $tagData)
+        foreach ($collectionTags as $serviceName => $tagData) {
             $routeCollections[] = $container->getDefinition($serviceName);
-
+        }
+        
         foreach ($routeTags as $routeServiceName => $tagData) {
             $routeNames = array();
-            foreach ($tagData as $tag)
-                if (isset($tag['route_name']))
+            foreach ($tagData as $tag) {
+                if (isset($tag['route_name'])) {
                     $routeNames[] = $tag['route_name'];
+                }
+            }
 
             if (!$routeNames)
                 continue;
 
             $routeReference = new Reference($routeServiceName);
-            foreach ($routeCollections as $collection)
-                foreach ($routeNames as $name)
+            foreach ($routeCollections as $collection) {
+                foreach ($routeNames as $name) {
                     $collection->addMethodCall('add', array($name, $routeReference));
+                }
+            }
         }
     }
 
